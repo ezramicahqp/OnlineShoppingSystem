d77af7e2214f2c6b3c8b115c6f220a596c6a95b6
maii <palamosemaq@gmail.com>
2024-05-16 11:33:51 +0800

    add cart page

---
+++ src/main/java/OnlineShoppingUI/cartPage.java
@@ -0,0 +1,13 @@
+
+package OnlineShoppingUI;
+
+/**
+ *
+ * @author My PC
+ */
+public class cartPage {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
