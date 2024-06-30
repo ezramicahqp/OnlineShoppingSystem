0c66e5945592dd062b5d77a9cf32525c4176a252
maii <palamosemaq@gmail.com>
2024-06-30 23:44:07 +0800

    fixed login page

--- src/main/java/OnlineShoppingModels/ProductInfo.java
+++ src/main/java/OnlineShoppingModels/ProductInfo.java
@@ -3,8 +3,10 @@ package OnlineShoppingModels;
 
 public class ProductInfo {
     
+    //public String ProductName;
     public String ProductName;
     public String ProductSize;
     public String ProductColor;
+    public String Description;
     public int ProductPrice;
 }
